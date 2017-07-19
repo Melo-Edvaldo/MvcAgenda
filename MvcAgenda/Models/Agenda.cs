fa9e18833c4ff9a00567cf2203268f4d289ba7fe
Edvaldo de Oliveira Melo <edvaldomelo@byui.edu>
2017-07-19 20:59:51 -0300

    Controller methods and views

--- MvcAgenda/Models/Agenda.cs
+++ MvcAgenda/Models/Agenda.cs
@@ -6,11 +6,21 @@ namespace MvcAgenda.Models
     public class Agenda
     {
         public int ID { get; set; }
+
+        [Display(Name = "Class Name")]
         public string ClassName { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        [Display(Name = "Class Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Hymn Name")]
         public string HymnName { get; set; }
+
+        [Display(Name = "First Prayer")]
         public string FirstPrayer { get; set; }
+
+        [Display(Name = "Last Prayer")]
         public string LastPrayer { get; set; }
     }
 }
